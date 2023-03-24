db59ecd217d51f7d4a699a2565090fcb37ecc493
TheOGChips <swindell.christian.g@gmail.com>
2023-03-23 20:15:45 -0500

    Initial build of documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'HAB-SEDS-TTU'
+copyright = '2023, William Stump, Chris Swindell, Luke Chapman, Emma Krebs, Mia Guzman'
+author = 'William Stump, Chris Swindell, Luke Chapman, Emma Krebs, Mia Guzman'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
